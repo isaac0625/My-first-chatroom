c41769c800ddeff2398b580be126873b2ac52c14
Kwok Chun Ho <kch062522@gmail.com>
2019-07-09 16:59:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ io.on('connection', function(socket){
     });
   });
 
-http.listen(3000,function(){
+http.listen(process.env.PORT || 5000,function(){
     console.log('listening on port 3000');
 });
