f424c8abad1ae0b64bc702fdece9e2c483d14c79
Kwok Chun Ho <kch062522@gmail.com>
2019-07-11 11:15:54 +0800

    Add features including save chat history in firebase.

--- index.js
+++ index.js
@@ -2,15 +2,46 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http); 
 
+var firebase = require("firebase/app");
+var firebase_database = require("firebase/database");
+var firebaseConfig = require('./firebaseServiceConfig');
+var fbaseApp = firebase.initializeApp(firebaseConfig);
+console.log("firebase app name: " + fbaseApp.name);
+var firebaseDB = fbaseApp.database();
+var ref_history = firebaseDB.ref('history');
+
+function firebase_pushNewMsg(msg) {
+    firebaseDB.ref('history').once('value').then(
+        function(snapshot){
+            var key = snapshot.numChildren();
+            var data = {};
+            data[key] = msg;
+            ref_history.update(data);
+        }
+    );
+}
+
 app.get('/', function(req, res){
+
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
     // io.emit('chat message', 'Someone has connected.');
+
+    // for()
+    firebaseDB.ref('history').once('value').then(
+        function(snapshot){
+            snapshot.forEach(function(snapshot_chatmessage){
+                io.emit('chat message', snapshot_chatmessage);
+            });
+        }
+    );
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
+        firebase_pushNewMsg(msg);
     });
+
     socket.on('disconnect', function(){
         // io.emit('chat message', 'Someone has disconnected.');
     });
